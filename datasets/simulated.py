ac8e56d900acdb41427e743fdf150cf9553785e5
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-21 14:56:52 +0100

    add simulated dataset

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,33 +1,25 @@
-import numpy as np
-
-from benchopt import BaseDataset
+from benchopt import BaseDataset, safe_import_context
+from benchopt.datasets import make_correlated_data
 
 
 class Dataset(BaseDataset):
 
     name = "Simulated"
 
-    # List of parameters to generate the datasets. The benchmark will consider
-    # the cross product for each key in the dictionary.
     parameters = {
         'n_samples, n_features': [
             (1000, 500),
-            (5000, 200)]
+            (500, 1000)
+        ]
     }
 
-    def __init__(self, n_samples=10, n_features=50, random_state=27):
-        # Store the parameters of the dataset
+    def __init__(self, n_samples, n_features, random_state=27):
         self.n_samples = n_samples
         self.n_features = n_features
         self.random_state = random_state
 
     def get_data(self):
+        X, y, _ = make_correlated_data(self.n_samples, self.n_features, rho=0.3,
+                                       random_state=self.random_state)
 
-        rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
-
-        # `data` holds the keyword arguments for the `set_data` method of the
-        # objective.
-        # They are customizable.
         return dict(X=X, y=y)
