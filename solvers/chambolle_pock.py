bb9cdd294023464393474e9a8cfed0a6772a5a7c
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-12-05 10:38:10 +0100

    chambolle pock

--- solvers/chambolle_pock.py
+++ solvers/chambolle_pock.py
@@ -3,49 +3,60 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
-    from skglm.prototype_pdcd.penalties import L1
-    from skglm.prototype_pdcd.datafits import SqrtQuadratic
-    from skglm.prototype_pdcd.algorithms import ChambollePock
+    from numba import njit
+    from benchmark_utils import prox_L1, prox_conjugate_L2
 
 
 class Solver(BaseSolver):
     name = "Chambolle-Pock"
 
     requirements = [
-        'pip:git+https://github.com/Badr-MOUFAD/skglm.git@pdcd-algo'
+        'pip:numba'
     ]
-
-    parameters = {
-        'with_dual_init': [True, False]
-    }
-
-    def __init__(self, with_dual_init):
-        self.with_dual_init = with_dual_init
-        self.dual_init = None
 
     def set_objective(self, X, y, lmbd):
         self.X, self.y, self.lmbd = X, y, lmbd
 
-        self.penalty = L1(lmbd)
-        self.datafit = SqrtQuadratic()
-
-        if self.with_dual_init:
-            self.dual_init = self.y / np.linalg.norm(y)
-
-        self.solver = ChambollePock(tol=1e-9, dual_init=self.dual_init)
-
         # Cache Numba compilation
-        self.run(5)
+        self.run(2)
 
     def run(self, n_iter):
         if n_iter == 0:
             self.coef = np.zeros([self.X.shape[1]])
         else:
             self.solver.max_iter = n_iter
-            coef = self.solver.solve(self.X, self.y,
-                                     self.datafit, self.penalty)[0]
+            coef = chambolle_pock(self.X, self.y, self.lmbd, n_iter)
 
             self.coef = coef.flatten()
 
     def get_result(self):
         return self.coef
+
+
+@njit
+def chambolle_pock(X, y, alpha=1., max_iter=1000):
+    n_samples, n_features = X.shape
+
+    # init steps
+    L = np.linalg.norm(X, ord=2)
+    dual_step = 0.99 / L
+    primal_step = 0.99 / L
+
+    # primal vars
+    w = np.zeros(n_features)
+    w_bar = np.zeros(n_features)
+
+    # dual vars
+    z = np.zeros(n_samples)
+
+    for _ in range(max_iter):
+        z[:] = prox_conjugate_L2(z + dual_step * X @ w_bar,
+                                 dual_step, y)
+
+        # primal update
+        old_w = w.copy()
+        w[:] = prox_L1(old_w - primal_step * X.T @ z,
+                       primal_step, alpha)
+        w_bar[:] = 2 * w - old_w
+
+        return w
