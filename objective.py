af5dd7f943082887b73516c88f12ed80efe2df46
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-12-09 15:14:48 +0100

    remove compute obj in objective

--- objective.py
+++ objective.py
@@ -18,8 +18,11 @@ class Objective(BaseObjective):
         self.lmbd = self.reg * Objective._compute_alpha_max(X, y)
 
     def compute(self, beta):
+        datafit_val = norm(self.y - self.X @ beta)
+        penalty_val = self.lmbd * norm(beta, ord=1)
+
         return {
-            'value': Objective._compute_p_obj(self.X, self.y, beta, self.lmbd),
+            'value': datafit_val + penalty_val,
             'support size': (beta != 0).sum()
         }
 
@@ -29,12 +32,6 @@ class Objective(BaseObjective):
     @staticmethod
     def _compute_alpha_max(X, y):
         return norm(X.T @ y, ord=np.inf) / norm(y)
-
-    @staticmethod
-    def _compute_p_obj(X, y, beta, lmbd):
-        datafit_val = norm(y - X @ beta)
-        penalty_val = lmbd * norm(beta, ord=1)
-        return datafit_val + penalty_val
 
     def get_one_solution(self):
         n_features = self.X.shape[1]
