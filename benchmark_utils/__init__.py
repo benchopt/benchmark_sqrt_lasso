b2fdb7725c42f23b19f47eefb206fa3e77f4d93b
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-12-09 13:53:25 +0100

    Revert "create new submodule"
    This reverts commit 32be1d13ecf5cf52ff33b15d982112b6bb06de3e.

---
+++ benchmark_utils/__init__.py
@@ -0,0 +1,25 @@
+import numpy as np
+from benchopt import safe_import_context
+
+with safe_import_context() as import_ctx:
+    from numba import njit
+
+
+@njit
+def prox_conjugate_L2(z, step, y):
+    # arg min_w ||y - w||^* + 1/(2*step) * ||w - z||^2
+
+    # project `u = z - step * y` on the L2 unit ball
+    u = z - step * y
+
+    norm_u = np.linalg.norm(u)
+    if norm_u <= 1.:
+        return u
+    return u / norm_u
+
+
+@njit
+def prox_L1(x, step, alpha):
+    # arg min_w ||w||_1 + 1/(2*step) ||w - z||^2
+    # entry-wise soft threshold
+    return np.sign(x) * np.maximum(0., np.abs(x) - step * alpha)
